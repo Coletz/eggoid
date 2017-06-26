15912dc4a7ae24c8d1370788dec2dec8ce303317
Dario Coletto Eggon <d.coletto@eggonemail.com>
2017-06-26 05:35:37 +0200

    Nil should now work with type inference

--- eggoid/src/main/java/co/eggon/eggoid/Nil.kt
+++ eggoid/src/main/java/co/eggon/eggoid/Nil.kt
@@ -1,33 +1,59 @@
 package co.eggon.eggoid
 
-class Nil(private vararg val objs: Any?){
 
-    infix fun <R> let(block: (Nil) -> R): R? {
-        return if(objs.all { it != null }){
-            block(this)
-        } else {
-            null
+class Nil<A, B>(val a: A, val b: B) {
+    operator fun component1(): A = a
+    operator fun component2(): B = b
+
+    infix fun let(callback: (Nil<A, B>) -> Unit): Nil<A, B> {
+        val objs = listOf(a, b)
+        if(objs.all { it != null }){
+            callback(this)
         }
+        return this
     }
 
-    operator fun component1(): Any? = objs[0]
-    operator fun component2(): Any? = objs[1]
-    operator fun component3(): Any? = objs[2]
-    operator fun component4(): Any? = objs[3]
-    operator fun component5(): Any? = objs[4]
-    operator fun component6(): Any? = objs[5]
-    operator fun component7(): Any? = objs[6]
-    operator fun component8(): Any? = objs[7]
-    operator fun component9(): Any? = objs[8]
-    operator fun component10(): Any? = objs[9]
-    operator fun component11(): Any? = objs[10]
-    operator fun component12(): Any? = objs[11]
-    operator fun component13(): Any? = objs[12]
-    operator fun component14(): Any? = objs[13]
-    operator fun component15(): Any? = objs[14]
-    operator fun component16(): Any? = objs[15]
-    operator fun component17(): Any? = objs[16]
-    operator fun component18(): Any? = objs[17]
-    operator fun component19(): Any? = objs[18]
-    operator fun component20(): Any? = objs[19]
+    infix fun empty(callback: () -> Unit) {
+        if(listOf(a, b).all { it != null }){
+            callback()
+        }
+    }
+}
+
+class Nil3<A, B, C>(val a: A, val b: B, val c: C) {
+    operator fun component1(): A = a
+    operator fun component2(): B = b
+
+    infix fun let(callback: (Nil3<A, B, C>) -> Unit): Nil3<A, B, C> {
+        if(listOf(a, b, c).all { it != null }){
+            callback(this)
+        }
+        return this
+    }
+
+    infix fun empty(callback: () -> Unit) {
+        val objs = listOf(a, b)
+        if(objs.all { it != null }){
+            callback()
+        }
+    }
+}
+
+class Nil4<A, B, C, D>(val a: A, val b: B, val c: C, val d: D) {
+    operator fun component1(): A = a
+    operator fun component2(): B = b
+
+    infix fun let(callback: (Nil4<A, B, C, D>) -> Unit): Nil4<A, B, C, D> {
+        if(listOf(a, b, c, d).all { it != null }){
+            callback(this)
+        }
+        return this
+    }
+
+    infix fun empty(callback: () -> Unit) {
+        val objs = listOf(a, b)
+        if(objs.all { it != null }){
+            callback()
+        }
+    }
 }
