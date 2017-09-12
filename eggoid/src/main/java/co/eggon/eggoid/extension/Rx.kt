f79bb12ec670b8ed5c0ee83183489f502cbf153a
Nicola Visentin Eggon <n.visentin@eggonemail.com>
2017-09-12 15:45:21 +0200

    Added rx utility

---
+++ eggoid/src/main/java/co/eggon/eggoid/extension/Rx.kt
@@ -0,0 +1,53 @@
+package co.eggon.eggoid.extension
+
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Action
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import okhttp3.MediaType
+import okhttp3.RequestBody
+
+/**
+ * Observable utilities
+ **/
+fun <T> Observable<T>.network(onNext: Consumer<T>, onError: Consumer<Throwable>, onComplete: Action? = null, onSubscribe: Consumer<Disposable>? = null): Disposable {
+    return onUi().async().let {
+        onSubscribe?.let {
+            subscribe(onNext, onError, onComplete, it)
+        } ?: onComplete?.let {
+            subscribe(onNext, onError, it)
+        } ?: run {
+            subscribe(onNext, onError)
+        }
+    }
+}
+
+fun <T> Observable<T>.network(onNext: ((T) -> Unit), onError: ((Throwable) -> Unit), onComplete: (() -> Unit)? = null, onSubscribe: ((Disposable) -> Unit)? = null): Disposable {
+    return onUi().async().let { obs ->
+        onSubscribe?.let {
+            obs.subscribe(onNext, onError, onComplete, it)
+        } ?: onComplete?.let {
+            obs.subscribe(onNext, onError, it)
+        } ?: run {
+            obs.subscribe(onNext, onError)
+        }
+    }
+}
+
+/**
+ * Execute the "subscribe" block (Conusmer) on the main thread, aka UI thread
+ **/
+fun <T> Observable<T>.onUi(): Observable<T> = observeOn(AndroidSchedulers.mainThread())
+
+/**
+ * Execute the observable on a new thread.
+ * Note: you can't execute any graphical change from thread different from the UI thread
+ **/
+fun <T> Observable<T>.async(): Observable<T> = subscribeOn(Schedulers.newThread())
+
+/**
+ * Create a request with text/plain media type
+ **/
+fun String.plainTextRequest(): RequestBody = RequestBody.create(MediaType.parse("text/plain"), this)
