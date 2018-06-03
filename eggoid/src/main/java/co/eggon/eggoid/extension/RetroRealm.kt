3bc8d4f2e1ace11b159f115f075615bad894ff65
Dario Coletto <coletz95@gmail.com>
2018-06-03 19:59:17 +0200

    Extensions duplicated for Single object

--- eggoid/src/main/java/co/eggon/eggoid/extension/RetroRealm.kt
+++ eggoid/src/main/java/co/eggon/eggoid/extension/RetroRealm.kt
@@ -11,9 +11,7 @@ import io.realm.exceptions.RealmException
 @Throws(RealmException::class)
 fun <E : RealmModel> Observable<E>.objectToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
     val promise = RealmPromise<E>()
-    this.subscribeOn(io.reactivex.schedulers.Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
+    this.network({
                 if (update) {
                     beforeSave?.invoke(it)
                     realm.update(it)
@@ -31,9 +29,7 @@ fun <E : RealmModel> Observable<E>.objectToRealm(realm: Realm?, update: Boolean 
 @Throws(RealmException::class)
 fun <E : RealmList<out RealmModel>> Observable<E>.listToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
     val promise = RealmPromise<E>()
-    this.subscribeOn(io.reactivex.schedulers.Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ list ->
+    this.network({ list ->
                 if (update) {
                     beforeSave?.invoke(list)
                     realm.update(list)
@@ -61,9 +57,7 @@ fun <E : RealmList<out RealmModel>> Observable<E>.listToRealm(realm: Realm?, upd
 @Throws(RealmException::class)
 fun <E : DataWrapper<out RealmModel>> Observable<E>.wrappedToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
     val promise = RealmPromise<E>()
-    this.subscribeOn(io.reactivex.schedulers.Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ wrapper ->
+    this.network({ wrapper ->
                 if (update) {
                     beforeSave?.invoke(wrapper)
                     wrapper.data?.let { data ->
@@ -97,9 +91,114 @@ fun <E : DataWrapper<out RealmModel>> Observable<E>.wrappedToRealm(realm: Realm?
 @Throws(RealmException::class)
 fun <E : DataListWrapper<out RealmModel>> Observable<E>.wrappedListToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
     val promise = RealmPromise<E>()
-    this.subscribeOn(io.reactivex.schedulers.Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ wrapper ->
+    this.network({ wrapper ->
+                if (update) {
+                    beforeSave?.invoke(wrapper)
+                    wrapper?.data?.let {
+                        realm.update(it)
+                                .then { promise.action?.invoke(wrapper) }
+                                .onError { promise.error?.invoke(it) ?: throw it }
+                    }
+                } else {
+                    wrapper?.data?.let {
+                        realm.create(it)
+                                .then { promise.action?.invoke(wrapper) }
+                                .onError { promise.error?.invoke(it) ?: throw it }
+                    }
+                }
+            }, {
+                promise.error?.invoke(it) ?: throw it
+            })
+    return promise
+}
+
+
+
+
+@Throws(RealmException::class)
+fun <E : RealmModel> Single<E>.objectToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
+    val promise = RealmPromise<E>()
+    this.network({
+                if (update) {
+                    beforeSave?.invoke(it)
+                    realm.update(it)
+                            .then { promise.action?.invoke(it) }
+                            .onError { promise.error?.invoke(it) ?: throw it }
+                } else {
+                    realm.create(it) then { promise.action?.invoke(it) } onError { promise.error?.invoke(it) ?: throw it }
+                }
+            }, {
+                promise.error?.invoke(it) ?: throw it
+            })
+    return promise
+}
+
+@Throws(RealmException::class)
+fun <E : RealmList<out RealmModel>> Single<E>.listToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
+    val promise = RealmPromise<E>()
+    this.network({ list ->
+                if (update) {
+                    beforeSave?.invoke(list)
+                    realm.update(list)
+                            .then { promise.action?.invoke(list) }
+                            .onError { promise.error?.invoke(it) ?: throw it }
+                } else {
+                    realm.create(list) then { promise.action?.invoke(list) } onError { promise.error?.invoke(it) ?: throw it }
+                }
+            }, {
+                promise.error?.invoke(it) ?: throw it
+            })
+    return promise
+}
+
+/**
+ * When using this function to unwrap an object that is returned as a field in the JSON,
+ * the class representing the response must implement the DataWrapper interface with the type of
+ * RealmObject you want to save.
+ *
+ * Example:
+ * class SomeResponse : DataWrapper<MyRealmObject> {
+ *     override var data: MyRealmObject? = null
+ * }
+ */
+@Throws(RealmException::class)
+fun <E : DataWrapper<out RealmModel>> Single<E>.wrappedToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
+    val promise = RealmPromise<E>()
+    this.network({ wrapper ->
+                if (update) {
+                    beforeSave?.invoke(wrapper)
+                    wrapper.data?.let { data ->
+                        realm.update(data)
+                                .then { promise.action?.invoke(wrapper) }
+                                .onError { promise.error?.invoke(it) ?: throw it }
+                    }
+                } else {
+                    wrapper.data?.let { data ->
+                        realm.create(data)
+                                .then { promise.action?.invoke(wrapper) }
+                                .onError { promise.error?.invoke(it) ?: throw it }
+                    }
+                }
+            }, {
+                promise.error?.invoke(it) ?: throw it
+            })
+    return promise
+}
+
+/**
+ * When using this function to unwrap a list of objects that is returned as a field in the JSON,
+ * the class representing the response must implement the DataListWrapper interface with the type of
+ * RealmObject you want to save.
+ *
+ * Example:
+ * class SomeResponse : DataListWrapper<MyRealmObject> {
+ *     override var data: RealmList<MyRealmObject>? = null
+ * }
+ */
+@Throws(RealmException::class)
+fun <E : DataListWrapper<out RealmModel>> Single<E>.wrappedListToRealm(realm: Realm?, update: Boolean = true, beforeSave: ((E) -> Unit)? = null): RealmPromise<E> {
+    val promise = RealmPromise<E>()
+    this.network({ wrapper ->
                 if (update) {
                     beforeSave?.invoke(wrapper)
                     wrapper?.data?.let {
