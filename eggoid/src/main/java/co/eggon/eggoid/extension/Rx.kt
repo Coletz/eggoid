e15bf16236ee698c3849b941e8d6341d742303a3
Dario Coletto <coletz95@gmail.com>
2018-06-03 19:50:43 +0200

    Added Single extension
    Network calls should always be single, not observable

--- eggoid/src/main/java/co/eggon/eggoid/extension/Rx.kt
+++ eggoid/src/main/java/co/eggon/eggoid/extension/Rx.kt
@@ -1,6 +1,7 @@
 package co.eggon.eggoid.extension
 
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Action
@@ -46,6 +47,26 @@ fun <T> Observable<T>.onUi(): Observable<T> = observeOn(AndroidSchedulers.mainTh
  * Note: you can't execute any graphical change from thread different from the UI thread
  **/
 fun <T> Observable<T>.async(): Observable<T> = subscribeOn(Schedulers.newThread())
+
+/**
+ * Single utilities
+ **/
+fun <T> Single<T>.network(bag: CompositeDisposable, onNext: Consumer<T>, onError: Consumer<Throwable>) 
+        = onUi().async().subscribe(onNext, onError).also { bag.add(it) }
+
+fun <T> Single<T>.network(bag: CompositeDisposable, onNext: ((T) -> Unit), onError: ((Throwable) -> Unit))
+        = onUi().async().subscribe(onNext, onError).also { bag.add(it) }
+
+/**
+ * Execute the "subscribe" block (Conusmer) on the main thread, aka UI thread
+ **/
+fun <T> Single<T>.onUi(): Single<T> = observeOn(AndroidSchedulers.mainThread())
+
+/**
+ * Execute the observable on a new thread.
+ * Note: you can't execute any graphical change from thread different from the UI thread
+ **/
+fun <T> Single<T>.async(): Single<T> = subscribeOn(Schedulers.newThread())
 
 /**
  * Create a request with text/plain media type
